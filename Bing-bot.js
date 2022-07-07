2f3b69f1b66ac655415352e5e0ed7518ed4df27e
HolyWave87 <107036333+HolyWave87@users.noreply.github.com>
2022-07-07 16:30:42 +0300

    update (Task 15)
    ищет, листает в поисковике, шагает по сайту и возвращаеться в поисковик и прочее

--- Bing-bot.js
+++ Bing-bot.js
@@ -5,6 +5,7 @@
 // @description  for Bing
 // @author       Grigorev Dmitriy
 // @match        https://www.bing.com/*
+// @match        https://auto.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -14,21 +15,55 @@ let keyword = keywords[getRandom(0, keywords.length)];
 let links = document.links;
 let bingInput = document.getElementsByName("q")[0];
 let bingSearch = document.getElementsByName("search")[0];
+let nextPage = document.getElementsByClassName("sb_pagS")[0];
+let numberPage = document.getElementsByClassName("sb_pagN")[0];
 
 
 if (bingSearch !== undefined) {
-  bingInput.value = keyword;
-  bingSearch.click();
-  } else {
-  for (let i = 0; i < links.length; i++) {
-    if (links[i].href.includes("auto.ru")) {
-    let link = links[i];
-    console.log("нашел строку " + links[i]);
-    link.click();
-    break;
+  let i = 0;
+  let timerId = setInterval(() => {
+    bingInput.value += keyword[i];
+    i++;
+    if(i == keyword.length) {
+      clearInterval(timerId);
+      bingSearch.click();
     }
+  },250);
+  }else if (location.hostname == "auto.ru") {
+
+    setInterval(() => {
+      let index = getRandom(0, links.length);
+
+      if (getRandom(0, 101) >= 85) {
+        location.href = "https://www.bing.com/";
+      } else if (links[index].href.indexOf("auto.ru") !== -1){
+        links[index].click();
+        }
+    }, getRandom(2000, 5000));
+  } else {
+    let nextBingPage = true;
+    for (let i = 0; i < links.length; i++) {
+      if (links[i].href.includes("https://auto.ru/")) {
+        let link = links[i];
+        nextBingPage = false;
+         setTimeout(() => {
+          link.click();
+        }, getRandom(2000, 5000));
+
+        break;
+      }
+    }
+    if (nextPage.innerText == 3) {
+      nextBingPage = false;
+      location.href = "https://www.bing.com/";
+    }
+    if (nextBingPage) {
+      setTimeout(() => {
+        numberPage.click();
+      }, getRandom(3000,7000));
+    }
+
   }
-}
 function getRandom (min, max) {
-  return Math.floor(Math.random()*(max - min) + min);
-  }
+  return Math.floor(Math.random() * (max - min) + min);
+}
