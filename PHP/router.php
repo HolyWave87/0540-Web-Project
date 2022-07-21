8f4199ac35915950d4f83e95254d746711f02f49
GrigorevDmitriy <107036333+HolyWave87@users.noreply.github.com>
2022-07-21 10:04:38 +0300

    for Learn

---
+++ PHP/router.php
@@ -0,0 +1,23 @@
+<?php
+
+$url = explode("/", $_SERVER['REQUEST_URI']);
+require_once("php/classes/User.php");
+
+if ($url[1] == "auth") {
+  $content = file_get_contents("pages/login.html");
+} else if ($url[1] == "reg") {
+  $content = file_get_contents("pages/register.html");
+} else if ($url[1] == "blog") {
+  $content = file_get_contents("pages/blog.html");
+} else if ($url[1] == "contact") {
+  $content = file_get_contents("pages/contact.php");
+} else if ($url[1] == "users") {
+  require_once("pages/users/index.html");
+} else if ($url[1] == "addUser") {
+  User::addUser();
+}
+else {
+  $content = file_get_contents("pages/index.php");
+}
+if (!empty($content))
+require_once("template.php");
